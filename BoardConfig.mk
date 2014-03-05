Quarx2k/android_device_moto_jordan-common

changes for safestrap

BoardConfig.mk
@@ -95,7 +95,7 @@ BUILD_PV_VIDEO_ENCODERS := 1
 BOARD_HAS_NO_SELECT_BUTTON := true
 TARGET_RECOVERY_FSTAB := device/moto/jordan-common/recovery.fstab
 RECOVERY_FSTAB_VERSION := 2
-TARGET_RECOVERY_INITRC := device/moto/jordan-common/profiles/ramdisk/init-recovery.rc
+TARGET_RECOVERY_INITRC := device/moto/jordan-common/ramdisk/init-recovery.rc
 BOARD_HAS_LARGE_FILESYSTEM := true
 BOARD_HAS_NO_MISC_PARTITION := true
 BOARD_USES_MMCUTILS := true
@@ -192,11 +192,11 @@ WLAN_MODULES:
 	arm-linux-androideabi-strip --strip-unneeded $(KERNEL_MODULES_OUT)/*
 
 hboot:
-	mkdir -p $(PRODUCT_OUT)/system/bootmenu/2nd-boot   
-	echo "$(BOARD_KERNEL_CMDLINE)" > $(PRODUCT_OUT)/system/bootmenu/2nd-boot/cmdline  
-	make -C  $(ANDROID_BUILD_TOP)/device/moto/jordan-common/hboot ARCH=arm CROSS_COMPILE="arm-eabi-"
+	mkdir -p $(PRODUCT_OUT)/system/bootstrap/2nd-boot   
+	echo "$(BOARD_KERNEL_CMDLINE)" > $(PRODUCT_OUT)/system//bootstrap/2nd-boot/cmdline  
+	make -C  $(ANDROID_BUILD_TOP)/device/moto/jordan-common/bootstrap/hboot ARCH=arm CROSS_COMPILE="arm-eabi-"
 	mv $(ANDROID_BUILD_TOP)/device/moto/jordan-common/bootstrap/hboot/hboot.bin $(PRODUCT_OUT)/system/bootstrap/2nd-boot/
-	make clean -C $(ANDROID_BUILD_TOP)/device/moto/jordan-common/hboot
+	make clean -C $(ANDROID_BUILD_TOP)/device/moto/jordan-common/bootstrap/hboot
 
 # If kernel sources are present in repo, here is the location
 TARGET_KERNEL_SOURCE := $(ANDROID_BUILD_TOP)/jordan-kernel

bootstrap/bootstrap.mk
@@ -2,17 +2,12 @@
 bm_device = device/moto/jordan-common
 include $(all-subdir-makefiles)
 
-# 2nd-boot profiles
-PRODUCT_COPY_FILES += \
-	${bm_device}/profiles/2nd-boot/hbootmod.ko:system/bootstrap/2nd-boot/hbootmod.ko \
-	${bm_device}/profiles/2nd-boot/hboot.cfg:system/bootstrap/2nd-boot/hboot.cfg \
-
 # Ramdisk
 PRODUCT_COPY_FILES += \
-	${bm_device}/profiles/ramdisk/ueventd.mapphone_umts.rc:root/ueventd.mapphone_umts.rc \
-	${bm_device}/profiles/ramdisk/init.usb.rc:root/init.usb.rc \
-	${bm_device}/profiles/ramdisk/init.mapphone_umts.rc:root/init.mapphone_umts.rc \
-	${bm_device}/profiles/ramdisk/fstab.mapphone_umts:root/fstab.mapphone_umts \
+	${bm_device}/ramdisk/ueventd.mapphone_umts.rc:root/ueventd.mapphone_umts.rc \
+	${bm_device}/ramdisk/init.usb.rc:root/init.usb.rc \
+	${bm_device}/ramdisk/init.mapphone_umts.rc:root/init.mapphone_umts.rc \
+	${bm_device}/ramdisk/fstab.mapphone_umts:root/fstab.mapphone_umts \
 
 # scripts
 PRODUCT_COPY_FILES += \
@@ -21,27 +16,23 @@ PRODUCT_COPY_FILES += \
 	${bm_device}/bootstrap/script/recovery.sh:system/bootstrap/script/recovery.sh \
 	${bm_device}/bootstrap/script/pdsbackup.sh:system/bootstrap/script/pdsbackup.sh \
 
-# prebuilt modules for stock kernel
-PRODUCT_COPY_FILES += \
-	${bm_device}/bootstrap/modules/jbd2.ko:system/bootstrap/modules/jbd2.ko \
-	${bm_device}/bootstrap/modules/ext4.ko:system/bootstrap/modules/ext4.ko \
-
-# images
-PRODUCT_COPY_FILES += \
-	${bm_device}/bootstrap/images/background-def.png:system/bootstrap/images/background-def.png \
-	${bm_device}/bootstrap/images/background-blank.png:system/bootstrap/images/background-blank.png \
-
 # prebuilt binaries
 PRODUCT_COPY_FILES += \
 	${bm_device}/bootstrap/binary/logwrapper:system/bin/logwrapper \
 	${bm_device}/bootstrap/binary/logwrapper.bin:system/bin/logwrapper.bin \
-	${bm_device}/profiles/2nd-boot/hboot_recovery.cfg:system/bootstrap/2nd-boot/hboot_recovery.cfg \
-	${bm_device}/bootstrap/binary/hbootuser:system/bootstrap/2nd-boot/hbootuser \
-	${bm_device}/bootstrap/binary/safestrapmenu:system/bootstrap/2nd-boot/safestrapmenu \
+	${bm_device}/bootstrap/binary/hbootuser:system/bootstrap/binary/hbootuser \
+	${bm_device}/bootstrap/binary/safestrapmenu:system/bootstrap/binary/safestrapmenu \
 	${bm_device}/bootstrap/binary/busybox:system/bootstrap/binary/busybox \
+	${bm_device}/bootstrap/modules/hbootmod.ko:system/bootstrap/modules/hbootmod.ko \
+	${bm_device}/bootstrap/2nd-boot/hboot.cfg:system/bootstrap/2nd-boot/hboot.cfg \
+	${bm_device}/bootstrap/2nd-boot/hboot_recovery.cfg:system/bootstrap/2nd-boot/hboot_recovery.cfg \
+	${bm_device}/bootstrap/2nd-boot/zImage-recovery:system/bootstrap/2nd-boot/zImage-recovery \
 	$(bm_device)/twrp.fstab:recovery/root/etc/twrp.fstab \
+	${bm_device}/bootstrap/modules/jbd2.ko:system/bootstrap/modules/jbd2.ko \
+	${bm_device}/bootstrap/modules/ext4.ko:system/bootstrap/modules/ext4.ko \
+	${bm_device}/bootstrap/images/background-def.png:system/bootstrap/images/background-def.png \
+	${bm_device}/bootstrap/images/background-blank.png:system/bootstrap/images/background-blank.png \
 	$(OUT)/ramdisk-recovery.img:system/bootstrap/2nd-boot/ramdisk-recovery \
-	${bm_device}/profiles/2nd-boot/zImage-recovery:system/bootstrap/2nd-boot/zImage-recovery \
 	$(OUT)/ramdisk.img:system/bootstrap/2nd-boot/ramdisk \
-	$(OUT)/kernel:system/bootstrap/2nd-boot/zImage
+	$(OUT)/kernel:system/bootstrap/2nd-boot/zImage \
 

MotoDX-CM11/android_device_moto_shadow-common

BoardConfig.mk
@@ -95,7 +95,7 @@ BUILD_PV_VIDEO_ENCODERS := 1
 BOARD_HAS_NO_SELECT_BUTTON := true
 TARGET_RECOVERY_FSTAB := device/moto/shadow-common/recovery.fstab
 RECOVERY_FSTAB_VERSION := 2
-TARGET_RECOVERY_INITRC := device/moto/shadow-common/profiles/ramdisk/init-recovery.rc
+TARGET_RECOVERY_INITRC := device/moto/shadow-common/ramdisk/init-recovery.rc
 BOARD_HAS_LARGE_FILESYSTEM := true
 BOARD_HAS_NO_MISC_PARTITION := true
 BOARD_USES_MMCUTILS := true
@@ -192,11 +192,11 @@ WLAN_MODULES:
 	arm-linux-androideabi-strip --strip-unneeded $(KERNEL_MODULES_OUT)/*
 
 hboot:
-	mkdir -p $(PRODUCT_OUT)/system/bootmenu/2nd-boot   
-	echo "$(BOARD_KERNEL_CMDLINE)" > $(PRODUCT_OUT)/system/bootmenu/2nd-boot/cmdline  
-	make -C  $(ANDROID_BUILD_TOP)/device/moto/shadow-common/hboot ARCH=arm CROSS_COMPILE="arm-eabi-"
+	mkdir -p $(PRODUCT_OUT)/system/bootstrap/2nd-boot   
+	echo "$(BOARD_KERNEL_CMDLINE)" > $(PRODUCT_OUT)/system//bootstrap/2nd-boot/cmdline  
+	make -C  $(ANDROID_BUILD_TOP)/device/moto/shadow-common/bootstrap/hboot ARCH=arm CROSS_COMPILE="arm-eabi-"
 	mv $(ANDROID_BUILD_TOP)/device/moto/shadow-common/bootstrap/hboot/hboot.bin $(PRODUCT_OUT)/system/bootstrap/2nd-boot/
-	make clean -C $(ANDROID_BUILD_TOP)/device/moto/shadow-common/hboot
+	make clean -C $(ANDROID_BUILD_TOP)/device/moto/shadow-common/bootstrap/hboot
 
 # If kernel sources are present in repo, here is the location
 TARGET_KERNEL_SOURCE := $(ANDROID_BUILD_TOP)/shadow-kernel

bootstrap/bootstrap.mk
@@ -2,17 +2,12 @@
 bm_device = device/moto/shadow-common
 include $(all-subdir-makefiles)
 
-# 2nd-boot profiles
-PRODUCT_COPY_FILES += \
-	${bm_device}/profiles/2nd-boot/hbootmod.ko:system/bootstrap/2nd-boot/hbootmod.ko \
-	${bm_device}/profiles/2nd-boot/hboot.cfg:system/bootstrap/2nd-boot/hboot.cfg \
-
 # Ramdisk
 PRODUCT_COPY_FILES += \
-	${bm_device}/profiles/ramdisk/ueventd.mapphone_cdma.rc:root/ueventd.mapphone_cdma.rc \
-	${bm_device}/profiles/ramdisk/init.usb.rc:root/init.usb.rc \
-	${bm_device}/profiles/ramdisk/init.mapphone_cdma.rc:root/init.mapphone_cdma.rc \
-	${bm_device}/profiles/ramdisk/fstab.mapphone_cdma:root/fstab.mapphone_cdma \
+	${bm_device}/ramdisk/ueventd.mapphone_cdma.rc:root/ueventd.mapphone_cdma.rc \
+	${bm_device}/ramdisk/init.usb.rc:root/init.usb.rc \
+	${bm_device}/ramdisk/init.mapphone_cdma.rc:root/init.mapphone_cdma.rc \
+	${bm_device}/ramdisk/fstab.mapphone_cdma:root/fstab.mapphone_cdma \
 
 # scripts
 PRODUCT_COPY_FILES += \
@@ -21,27 +16,23 @@ PRODUCT_COPY_FILES += \
 	${bm_device}/bootstrap/script/recovery.sh:system/bootstrap/script/recovery.sh \
 	${bm_device}/bootstrap/script/pdsbackup.sh:system/bootstrap/script/pdsbackup.sh \
 
-# prebuilt modules for stock kernel
-PRODUCT_COPY_FILES += \
-	${bm_device}/bootstrap/modules/jbd2.ko:system/bootstrap/modules/jbd2.ko \
-	${bm_device}/bootstrap/modules/ext4.ko:system/bootstrap/modules/ext4.ko \
-
-# images
-PRODUCT_COPY_FILES += \
-	${bm_device}/bootstrap/images/background-def.png:system/bootstrap/images/background-def.png \
-	${bm_device}/bootstrap/images/background-blank.png:system/bootstrap/images/background-blank.png \
-
 # prebuilt binaries
 PRODUCT_COPY_FILES += \
 	${bm_device}/bootstrap/binary/logwrapper:system/bin/logwrapper \
 	${bm_device}/bootstrap/binary/logwrapper.bin:system/bin/logwrapper.bin \
-	${bm_device}/profiles/2nd-boot/hboot_recovery.cfg:system/bootstrap/2nd-boot/hboot_recovery.cfg \
-	${bm_device}/bootstrap/binary/hbootuser:system/bootstrap/2nd-boot/hbootuser \
-	${bm_device}/bootstrap/binary/safestrapmenu:system/bootstrap/2nd-boot/safestrapmenu \
+	${bm_device}/bootstrap/binary/hbootuser:system/bootstrap/binary/hbootuser \
+	${bm_device}/bootstrap/binary/safestrapmenu:system/bootstrap/binary/safestrapmenu \
 	${bm_device}/bootstrap/binary/busybox:system/bootstrap/binary/busybox \
+	${bm_device}/bootstrap/modules/hbootmod.ko:system/bootstrap/modules/hbootmod.ko \
+	${bm_device}/bootstrap/2nd-boot/hboot.cfg:system/bootstrap/2nd-boot/hboot.cfg \
+	${bm_device}/bootstrap/2nd-boot/hboot_recovery.cfg:system/bootstrap/2nd-boot/hboot_recovery.cfg \
+	${bm_device}/bootstrap/2nd-boot/zImage-recovery:system/bootstrap/2nd-boot/zImage-recovery \
 	$(bm_device)/twrp.fstab:recovery/root/etc/twrp.fstab \
+	${bm_device}/bootstrap/modules/jbd2.ko:system/bootstrap/modules/jbd2.ko \
+	${bm_device}/bootstrap/modules/ext4.ko:system/bootstrap/modules/ext4.ko \
+	${bm_device}/bootstrap/images/background-def.png:system/bootstrap/images/background-def.png \
+	${bm_device}/bootstrap/images/background-blank.png:system/bootstrap/images/background-blank.png \
 	$(OUT)/ramdisk-recovery.img:system/bootstrap/2nd-boot/ramdisk-recovery \
-	${bm_device}/profiles/2nd-boot/zImage-recovery:system/bootstrap/2nd-boot/zImage-recovery \
 	$(OUT)/ramdisk.img:system/bootstrap/2nd-boot/ramdisk \
-	$(OUT)/kernel:system/bootstrap/2nd-boot/zImage
+	$(OUT)/kernel:system/bootstrap/2nd-boot/zImage \
 