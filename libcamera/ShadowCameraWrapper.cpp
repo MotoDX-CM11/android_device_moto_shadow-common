cfa4b022fabe88a6d620b2d5959bb342047712b4
mksy <maksik.boy@gmail.com>
2014-04-07 07:05:48 +0300

    fix 720p record (6Mp autofocus broken again)

--- libcamera/ShadowCameraWrapper.cpp
+++ libcamera/ShadowCameraWrapper.cpp
@@ -413,10 +413,10 @@ ShadowCameraWrapper::getParameters() const
     ret.set(CameraParameters::KEY_MIN_EXPOSURE_COMPENSATION, "-9");
     ret.set(CameraParameters::KEY_EXPOSURE_COMPENSATION_STEP, "0.3333333333333");
     ret.set(CameraParameters::KEY_VIDEO_FRAME_FORMAT, CameraParameters::PIXEL_FORMAT_YUV422I);
-    ret.set(CameraParameters::KEY_PREVIEW_FRAME_RATE, "24");
-    ret.set(CameraParameters::KEY_SUPPORTED_PREVIEW_FPS_RANGE, "(15000,30000),(15000,25000),(15000,20000),(15000,24000),(15000,15000)");
+    //ret.set(CameraParameters::KEY_PREVIEW_FRAME_RATE, "24");
+    ret.set(CameraParameters::KEY_SUPPORTED_PREVIEW_FPS_RANGE, "(15000,30000)");
     ret.set(CameraParameters::KEY_PREVIEW_FPS_RANGE, "15000,30000");
-    ret.set(CameraParameters::KEY_SUPPORTED_PREVIEW_SIZES, "640x480");
+    //ret.set(CameraParameters::KEY_SUPPORTED_PREVIEW_SIZES, "640x480");
     ret.set(CameraParameters::KEY_SUPPORTED_VIDEO_SIZES, "");
 
     ret.set("cam-mode", mVideoMode ? "1" : "0");
