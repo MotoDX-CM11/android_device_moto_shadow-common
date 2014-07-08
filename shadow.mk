b4470bafac7e4ff2f0c6ffe8c4beaa8ab090f67e
mksy <maksik.boy@gmail.com>
2014-07-08 20:39:36 +0300

    enable 720p encoder from sources

--- shadow.mk
+++ shadow.mk
@@ -127,6 +127,7 @@ PRODUCT_PACKAGES += dspexec libbridge libLCML libOMX_Core libstagefrighthw
 PRODUCT_PACKAGES += libOMX.TI.AAC.encode libOMX.TI.AAC.decode libOMX.TI.AMR.decode libOMX.TI.AMR.encode
 PRODUCT_PACKAGES += libOMX.TI.WBAMR.encode libOMX.TI.MP3.decode libOMX.TI.WBAMR.decode
 PRODUCT_PACKAGES += libOMX.TI.Video.Decoder libOMX.TI.Video.encoder libOMX.TI.JPEG.Encoder
+PRODUCT_PACKAGES += libOMX.TI.720P.Encoder
 
 # Droid X stuff
 PRODUCT_PACKAGES += libfnc DXParts MotoFM MotoFMService HwaSettings
