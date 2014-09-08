34ee08bf7c60e1d29eec785b3a10cea661049fcd
mksy <maksik.boy@gmail.com>
2014-09-08 05:21:04 +0300

    Remove egl/hwui props

--- shadow.mk
+++ shadow.mk
@@ -30,20 +30,6 @@ $(call inherit-product, frameworks/native/build/phone-hdpi-512-dalvik-heap.mk)
 
 DEVICE_PACKAGE_OVERLAYS += device/moto/shadow-common/overlay
 
-#HWUI Tweaks
-PRODUCT_PROPERTY_OVERRIDES += \
-	ro.hwui.disable_scissor_opt=true \
-	ro.hwui.texture_cache_size=4 \
-	ro.hwui.fbo_cache_size=4 \
-	ro.hwui.text_small_cache_width=256 \
-	ro.hwui.text_small_cache_height=64 \
-	ro.hwui.text_large_cache_width=512 \
-	ro.hwui.text_large_cache_height=144 \
-	ro.hwui.drop_shadow_cache_size=0.5 \
-	ro.hwui.r_buffer_cache_size=1 \
-	ro.hwui.text_gamma_correction=shader \
-	hwui.use_gpu_pixel_buffers=false
-
 PRODUCT_PROPERTY_OVERRIDES += \
 	ro.media.capture.flip=horizontalandvertical \
 	ro.com.google.locationfeatures=1 \
@@ -53,17 +39,12 @@ PRODUCT_PROPERTY_OVERRIDES += \
 	ro.opengles.version=131072 \
 	hwui.use.blacklist=true \
 	ro.sf.lcd_density=240 \
-	ro.bq.gpu_to_cpu_unsupported=1 \
 	dalvik.vm.debug.alloc=0 \
 	ro.setupwizard.enable_bypass=1 \
 	persist.sys.root_access=3 \
 	ro.config.low_ram=true  \
 	dalvik.vm.jit.codecachesize=0 \
 	ro.input.noresample=1 \
-	debug.sf.fb_always_on=1 \
-	debug.egl.hw=1 \
-	debug.sf.hw=1 \
-	debug.sf.no_hw_vsync=1
 #	cm.updater.uri=http://defy.cm-for.us/api \
 
 # Set default USB interface
